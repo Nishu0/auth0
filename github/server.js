f3d7cb55603231107a7291637b811e0375c16d82
Nishu0 <89217455+Nishu0@users.noreply.github.com>
2022-06-21 15:57:43 +0530

    Create server.js

---
+++ github/server.js
@@ -0,0 +1,53 @@
+/*  EXPRESS */
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+var access_token = "";
+
+app.get('/', function(req, res) {
+  res.render('pages/index',{client_id: clientID});
+});
+
+const PORT = process.env.PORT || 5000;
+
+
+const axios = require('axios')
+// This is the client ID and client secret that you obtained
+// while registering on github app
+const clientID = '##########'
+const clientSecret = '#########'
+
+// Declare the callback route
+app.get('/github/callback', (req, res) => {
+
+  // The req.query object has the query params that were sent to this route.
+  const requestToken = req.query.code
+  
+  axios({
+    method: 'post',
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+    // Set the content type header, so that we get the response in JSON
+    headers: {
+         accept: 'application/json'
+    }
+  }).then((response) => {
+    access_token = response.data.access_token
+    res.redirect('/success');
+  })
+})
+
+app.get('/success', function(req, res) {
+  axios({
+    method: 'get',
+    url: `https://api.github.com/user`,
+    headers: {
+      Authorization: 'token ' + access_token
+    }
+  })
+  .then((response) => {
+    res.render('pages/success',{ userData: response.data });
+  })
+});
+
+app.listen(PORT , () => console.log(`The port is on ${PORT}`));
